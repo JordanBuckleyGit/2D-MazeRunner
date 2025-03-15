afa99f31a1382ec09f9b18eba94187801a1708ef
JordanBuckleyGit <jordanbuckleycork@gmail.com>
2025-03-15 17:44:48 +0000

    text

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, session, redirect, url_for, g, request
+from database import get_db, close_db
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import #add forms here
+from functools import wraps
+
+
+
+app = Flask(__name__)
+app.teardown_appcontext(close_db)
+app.config["SECRET_KEY"] = "jordansPw"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#############
+# server side 
+##############
+
+@app.before_request
+def logged_in_user():
+    g.user = session.get("user_id", None)
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for("login", next=request.url))
+        return view(*args, **kwargs)
+    return wrapped_view
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
