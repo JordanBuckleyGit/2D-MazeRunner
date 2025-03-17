851d042d9c118adb0d4f66b54001348bc237eb95
JordanBuckleyGit <jordanbuckleycork@gmail.com>
2025-03-17 09:15:38 +0000

    more basics

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, g, request
 from database import get_db, close_db
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import #add forms here
+from forms import LoginForm, RegistrationForm
 from functools import wraps
 
 
@@ -30,13 +30,71 @@ def login_required(view):
         return view(*args, **kwargs)
     return wrapped_view
 
-@app.route("/")
+
+#################
+# login/register
+#################
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        password = form.password.data
+        db = get_db()
+
+        user_in_db = db.execute("SELECT * FROM users WHERE user_id = ?;", (user_id,)).fetchone()
+        if user_in_db:
+            form.user_id.errors.append("User ID already exists! Please choose a different one.")
+        else:
+            hashed_password = generate_password_hash(password)
+            db.execute("INSERT INTO users (user_id, password) VALUES (?, ?);", (user_id, hashed_password))
+            db.commit()
+            return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_id = form.user_id.data
+        password = form.password.data
+        db = get_db()
+
+        # check if the user exists
+        user_in_db = db.execute("SELECT * FROM users WHERE user_id = ?;", (user_id,)).fetchone()
+        if user_in_db is None:
+            form.user_id.errors.append("No such user name!")
+        elif not check_password_hash(user_in_db["password"], password):
+            form.password.errors.append("Incorrect password!")
+        else:
+            # log the user in
+            session.clear()
+            session["user_id"] = user_id
+            session.modified = True
+            next_page = request.args.get("next")
+
+            if not next_page:
+                next_page = url_for("index")
+            return redirect(next_page)
+    return render_template("login.html", form=form)
+
+@app.route("/logout")
 @login_required
+def logout():
+    session.clear()
+    session.modified = True 
+    return redirect( url_for("index"))
+
+###########
+# basics
+############
+
+@app.route("/")
 def index():
     return render_template("base.html")
 
 @app.route("/game")
-@login_required
 def game():
     return render_template("game.html")
 
