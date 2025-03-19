abbb3ce2334de3f806d66379072915b5e4092e38
JordanBuckleyGit <jordanbuckleycork@gmail.com>
2025-03-19 18:06:43 +0000

    some fixes

--- schema.sql
+++ schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     user_id TEXT PRIMARY KEY,
     password TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS leaderboards;
+
+CREATE TABLE leaderboards (
+    user_id TEXT PRIMARY KEY,
+    score INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
